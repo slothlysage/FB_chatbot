28d8c67cc262e66b15b366b1d9a45192fb11a3ab
seanjones2848 <seanjones2848@gmail.com>
2018-04-29 01:12:23 -0700

    basic azure bot

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=hi-teresa-Web-Deploy.pubxml -p:Password=GoB5lXpL9yCEnejSu5R12dFhqA3pdaS3bwY8nE1mNcFuTtpvRLEECwqp3x45
+
